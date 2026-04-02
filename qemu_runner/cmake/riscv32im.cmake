246a6425c6d4712706decae4b3274d376659a78b
Somnath Banerjee <snb895@outlook.com>
2026-04-02 18:59:03 +0400

    Unify CMakeLists for qemu_runner (#78)

--- qemu_runner/cmake/riscv32im.cmake
+++ qemu_runner/cmake/riscv32im.cmake
@@ -12,11 +12,11 @@ set(CMAKE_ASM_COMPILER riscv-none-elf-gcc)
 # set(GMP_INCLUDE_DIR "${CMAKE_SOURCE_DIR}/../prelibs/gmp")
 set(BUILD_SHARED_LIBS "OFF")
 
-# set(debug_flags "-Os")
-set(debug_flags "-O0 -g3")
 set(common_flags "-march=rv32im -mabi=ilp32 -mcmodel=medany -ffunction-sections -fdata-sections -fPIC") #"-ffreestanding"
 set(CMAKE_C_FLAGS "${common_flags} -D_GLIBCXX_HAS_GTHREADS=0")
-set(CMAKE_CXX_FLAGS "${common_flags} ${debug_flags} -fno-exceptions -fno-rtti -fno-threadsafe-statics ")  # "-nostdlib"
+set(CMAKE_CXX_FLAGS "${common_flags} -fno-exceptions -fno-rtti -fno-threadsafe-statics ")  # "-nostdlib"
+set(CMAKE_CXX_FLAGS_DEBUG "-O0 -g3")
+set(CMAKE_ASM_FLAGS_DEBUG "-g3")
 
 
 set(BUILD_TESTING OFF CACHE BOOL "Disable Silkworm tests")
